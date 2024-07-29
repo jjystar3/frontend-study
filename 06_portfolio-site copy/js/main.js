4f2cdd082a2f6913beb9146834695553d5923f18
JuYoung <jjystar3@gmail.com>
2024-07-29 17:43:36 +0900

    added jQuery element detection

--- 06_portfolio-site copy/js/main.js
+++ 06_portfolio-site copy/js/main.js
@@ -1,15 +1,17 @@
 // ScrollMagic 사용법
-let spyEls = document.querySelectorAll('section.scroll-spy');
-console.log(spyEls);
+// let spyEls = document.querySelectorAll('section.scroll-spy');
+// console.log(spyEls);
 
-spyEls.forEach(function (spyEl) {
-  new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
-    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.5// 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
-  })
-  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
-  .addTo(new ScrollMagic.Controller());
-});
+// spyEls.forEach(function (spyEl) {
+//   new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
+//     triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+//     triggerHook: 0.5// 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
+//   })
+//   .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+//   .addTo(new ScrollMagic.Controller());
+// });
+
+
 
 
 
@@ -37,8 +39,6 @@ thisYear.textContent = new Date().getFullYear();
 
 // 페이지 최상단으로 이동
 let toTopBtn = document.querySelector('#to-top');
-let bgImg = document.querySelector('.bg-img');
-console.log(bgImg);
 
 // 페이지에 스크롤 이벤트 감지를 추가
 // window: 브라우저 창 객체
@@ -56,16 +56,15 @@ window.addEventListener('scroll', function () {
     toTopBtn.style.opacity = 0;
     toTopBtn.style.transform = 'translateX(100px)';
   }
-  if(window.scrollY < 500 || window.scrollY > 3000){  
-    bgImg.style.filter = 'blur(0)';  
-  }else{
-    bgImg.style.filter = 'blur(10px)';
-  }
+
+
 });
 
 
 let bgDay = document.querySelector('.bg-day');
 let bgNight = document.querySelector('.bg-night');
+
+let bgImg = document.querySelector('.bg-img');
 
 $(document).ready(function() {
 
@@ -77,9 +76,22 @@ $(document).ready(function() {
     var scrollPercentRounded = Math.round(scrollPercent*100);
 
       // $('#scrollPercentLabel>span').html(scrollPercentRounded);
-      console.log(scrollPercentRounded);
-      bgDay.style.opacity = 1 - (scrollPercentRounded * 0.01);
+      // console.log(scrollPercentRounded);
+      // bgDay.style.opacity = 1 - (scrollPercentRounded * 0.01);
       bgNight.style.opacity = scrollPercentRounded * 0.01;
+
+      if(scrollPercentRounded < 10 || scrollPercentRounded > 90){
+        bgImg.style.filter = 'blur(0)';  
+      }else{
+        bgImg.style.filter = 'blur(10px)';  
+      }
+
+      if (checkVisible($('.skill-spy'))) {
+        $('.skill-spy').addClass("show");
+      } else {
+        $('.skill-spy').removeClass("show");
+      }
+
       repositionLabel();
   });
 
@@ -98,3 +110,25 @@ $(document).ready(function() {
   repositionLabel();
 
 });
+
+
+// $(window).scroll(function() {
+//   if (checkVisible($('section.scroll-spy'))) {
+    
+//       element.classList.add("show");
+//   } else {
+    
+//   }
+// });
+
+
+function checkVisible( elm, eval ) {
+  eval = eval || "visible";
+  var vpH = $(window).height(), // Viewport Height
+      st = $(window).scrollTop(), // Scroll Top
+      y = $(elm).offset().top,
+      elementHeight = $(elm).height();
+  
+  if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+  if (eval == "above") return ((y < (vpH + st)));
+}
